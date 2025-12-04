65293b56bd8de80e9cc254ab01eb8855a71c625d
sorozco <soorozco2027@gmail.com>
2025-12-04 14:27:39 -0800

    updated version of YAAA

--- Youreawesome/ContentView.swift
+++ Youreawesome/ContentView.swift
@@ -10,46 +10,54 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
      
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.blue)
+                            .multilineTextAlignment(.center)
+                            .minimumScaleFactor(0.5)
+                            .frame(height:120)
+                            .animation (.easeInOut(duration: 0.15), value: message)
             
+
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.light)
-                .foregroundStyle(.blue)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("show message") {
                 let messages = [ "You Are Awesome!",
+                                 "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
                                  "Your Are Great!",
                                  "You Are Fantastic!",
-                                 "Fabulous? That's You!"]
+                                 "Fabulous? That's You!",
+                " You make me Smile!"]
                 
-                message = messages[ messageNumber]
-                messageNumber += 1
-                if messageNumber == 4 {
-                        messageNumber = 0
-        
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                    
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber{
+                    imageNumber = Int.random(in: 0...9)
                 }
-                
-                
                 imageName = "image\(imageNumber)"
-                imageNumber += 1
+                lastImageNumber = imageNumber
                 
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title)
